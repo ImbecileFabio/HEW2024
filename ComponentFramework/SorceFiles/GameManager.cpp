dcdfdbfa08a6ed3fddc93f9158b090f3ca61dbe9
ふぁび <ImbecileFabio@gmail.com>
2024-11-04 21:55:11 +0900

    iter_swap変えた
    ranges::iter_swapにしたらいけた
    c++20の機能っぽいから留意されたし
    from::desktop

--- ComponentFramework/SorceFiles/GameManager.cpp
+++ ComponentFramework/SorceFiles/GameManager.cpp
@@ -130,7 +130,7 @@ void GameManager::RemoveGameObject(GameObject* gameObject)
 	if (iter != game_objects_.end())
 	{
 		// 一致する"gameObject"をコンテナの末尾へ移動させ、メモリ自体を破棄する
-		std::iter_swap(iter, game_objects_.end() - 1);
+		std::ranges::iter_swap(iter, game_objects_.end() - 1);
 		game_objects_.pop_back();
 	}
 }
