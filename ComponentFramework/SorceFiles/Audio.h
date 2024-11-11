79a067a178a7fd657990258996ef3dbe5a6cdaf0
ryoya-nakatani <ohs30558@ohs.hal.ac.jp>
2024-11-12 00:42:26 +0900

    Xaudio2のやつ
    DXで配布されたsoundファイルを基に理解できたところまで追加してみました。
    ファイル名とりあえずでつけたので変更希望あればお願いします。

---
+++ ComponentFramework/SorceFiles/Audio.h
@@ -0,0 +1,46 @@
+//==================================================
+// [Audio.h] オーディオオブジェクト
+// 著者：中谷凌也
+//--------------------------------------------------
+// 説明：サウンドの制御をおこなうクラス
+//==================================================
+#pragma once
+#include <xaudio2.h>
+#include <iostream>
+
+// サウンドファイル
+typedef enum
+{
+	SoundLabelBgm000 = 0,		// 仮置きBGM
+//	SoundLabel○○○,			// この形で列挙していく予定
+								// ↓
+								// ↓
+
+	SoundLabelMax,				// サウンドの総数
+} SoundLabel;
+
+class Audio
+{
+private:
+	// パラメータ構造体
+	typedef struct
+	{
+		LPCSTR fileName;	// 音声ファイルまでのパスを設定
+		bool bLoop;			// trueでループ。通常BGMはture、SEはfalse。
+	} Param;
+
+	// パラメーター構造体の初期化
+	Param param[SoundLabelMax] = {
+		{ "○○○/○○○/○○○.wav", true },	// 仮置き音源
+		// ↓
+		// ↓
+
+	};	
+
+public:
+    HRESULT Init();
+	void Uninit();
+	void Play(SoundLabel _label);
+	void Stop(SoundLabel _label);
+};
+
