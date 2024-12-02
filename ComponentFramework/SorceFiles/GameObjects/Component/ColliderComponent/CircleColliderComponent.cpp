9b03add74f5cbdad694b9c30a211a9596eb242e8
nemekoMK <ohs30721@ohs.hal.ac.jp>
2024-12-02 11:30:35 +0900

    warningの抜けを修正
    2024/12/02 11:30

--- ComponentFramework/SorceFiles/GameObjects/Component/ColliderComponent/CircleColliderComponent.cpp
+++ ComponentFramework/SorceFiles/GameObjects/Component/ColliderComponent/CircleColliderComponent.cpp
@@ -59,6 +59,7 @@ bool CircleColliderComponent::CheckCollisionCollider(CircleColliderComponent* _o
 		this->hitFg = true;
 		return true;
 	}
+	return false;
 }
 //--------------------------------------------------
 // @brief ‰~‚ÆlŠp‚Ì“–‚½‚è”»’è‚ğ‚Æ‚éŠÖ”
