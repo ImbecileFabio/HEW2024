f37b8faa408c276a2b0c953253771da5778e5e9d
ryoya-nakatani <ohs30558@ohs.hal.ac.jp>
2024-12-13 22:47:47 +0900

    ペンデュラムマネージャーちょい

--- ComponentFramework/SorceFiles/PemdulumManager.h
+++ ComponentFramework/SorceFiles/PemdulumManager.h
@@ -2,18 +2,41 @@
 // [PemdulumManager.h] 
 // 著者：中谷凌也
 //--------------------------------------------------
-// 説明：説明：振り子の管理（選択）
+// 説明：振り子の管理（選択）
+//  		決定
+//		選択
+//		状態遷移
+//		長さの変更
 //==================================================
 #pragma once
 #include "GameObjects/GameObject.h"
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
+#include <SimpleMath.h>
+
+#define NormalLangth 200
+#define LangthChange 50
 
 class PemdulumManager
 {
 private:
+	// 振り子の長さの状態
+	enum class LangthState
+	{
+		shortLangth		= NormalLangth - LangthChange,
+		normalLangth	= NormalLangth,
+		longLangth		= NormalLangth + LangthChange
+	};
+	LangthState langthState_;
+	bool pemdulumMovement_;
+
+	DirectX::SimpleMath::Vector3 pemdulumPosition_;
+	DirectX::SimpleMath::Vector2 stickVector_;
+	DirectX::SimpleMath::Vector2 pemdulumVector_;
+
+
 	std::list<GameObject*> pemgulumList_;
-	GameObject* pSelectedObject;
+	GameObject* pSelectedPemdulum;
 
 public:
 	PemdulumManager();
@@ -23,6 +46,7 @@ public:
 	void Uninit();
 	void Update();
 
-	void AddGameObject(GameObject* _object);
 	 GameObject* PemgulumSelect(float _inputStick);
+	 void PemdulumMovementChange();
+	 void PemgulumLangthChange();
 };
