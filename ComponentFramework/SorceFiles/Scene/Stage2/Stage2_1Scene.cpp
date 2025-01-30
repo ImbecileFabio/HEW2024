0d0f781c35450477a7ea3ca4a17640e8aec6c214
ueno0309 <164988834+ueno0309@users.noreply.github.com>
2025-01-30 19:14:14 +0900

    1のステージはほぼ完成です！

--- ComponentFramework/SorceFiles/Scene/Stage2/Stage2_1Scene.cpp
+++ ComponentFramework/SorceFiles/Scene/Stage2/Stage2_1Scene.cpp
@@ -22,8 +22,8 @@
 #include "../../GameObjects/Component/ChildrenComponent.h"
 
 
-constexpr int gearCounter_2_1 = 1;		// ƒMƒA‚ÌŠl“¾”
-constexpr int hammerCounter_2_1 = 3;	// ’@‚¯‚éãŒÀ
+constexpr int gearCounter_2_1 = 2;		// ƒMƒA‚ÌŠl“¾”
+constexpr int hammerCounter_2_1 = 2;	// ’@‚¯‚éãŒÀ
 //--------------------------------------------------
 // ƒRƒ“ƒXƒgƒ‰ƒNƒ^
 //--------------------------------------------------
